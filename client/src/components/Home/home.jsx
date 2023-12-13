2aacd677fbdf28937879c2bd4a602c4443406d6b
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2023-12-13 13:26:26 +0530

    sameer added google map

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
-
+import { FaMapMarkerAlt } from "react-icons/fa";
 import logo from "../../assets/images/logo.png";
 import grid from "../../assets/images/grid.png";
 import mini from "../../assets/images/mini.png";
@@ -14,7 +14,28 @@ import facebook from "../../assets/images/icon _facebook_facebook.png";
 
 const Home = () => {
   const navigate = useNavigate();
+  const openInstagram = () => {
+    window.open(
+      "https://www.instagram.com/binge__in?igshid=MmVlMjlkMTBhMg%3D%3D",
+      "_blank"
+    );
+  };
 
+  const openFacebook = () => {
+    window.open("https://www.facebook.com/your_facebook_page/", "_blank");
+  };
+
+  const openYoutube = () => {
+    window.open("https://www.youtube.com/your_youtube_channel/", "_blank");
+  };
+
+  const openCall = () => {
+    // Replace the phone number with the actual phone number
+    window.open("tel:+918074020058");
+  };
+  const openLocation = () => {
+    window.open("https://maps.app.goo.gl/b5xtHs2DNd3oRuVX6?g_st=ic", "_blank");
+  };
   return (
     <>
       <div className="Beigein-home-con">
@@ -47,10 +68,12 @@ const Home = () => {
               <p>
                 {/* Experience intimate cinematic moments and celebrate in style
                 within our mini private theatre's exclusive ambiance. */}
-                1799 for 4 or less people<br/>
-                399 per extra person <br/>
-                Decoration is included <br/>
-                you can get your own food and cake<br/>
+                1799 for 4 or less people
+                <br />
+                399 per extra person <br />
+                Decoration is included <br />
+                you can get your own food and cake
+                <br />
               </p>
               <button
                 onClick={() => {
@@ -66,10 +89,11 @@ const Home = () => {
                 {/* Immerse yourself in grand cinematic experiences and elevate
                 celebrations within our spacious and versatile large private
                 theatre. */}
-                2999 for 6 or less people <br/>
-                More that 4 people 399 per person <br/>
-                Decoration is included in the price <br/>
-                you can get your own food and cake<br/>
+                2999 for 6 or less people <br />
+                More that 4 people 399 per person <br />
+                Decoration is included in the price <br />
+                you can get your own food and cake
+                <br />
               </p>
               <button
                 onClick={() => {
@@ -103,10 +127,14 @@ const Home = () => {
               </p>
             </div>
             <div className="socialmedia-links">
-              <img src={insta} alt="insta" />
-              <img src={facebook} alt="facebook" />
-              <img src={youtube} alt="youtube" />
-              <img src={call} alt="call" />
+              <img src={insta} alt="insta" onClick={openInstagram} />
+              <img src={facebook} alt="facebook" onClick={openFacebook} />
+              <img src={youtube} alt="youtube" onClick={openYoutube} />
+              <img src={call} alt="call" onClick={openCall} />
+              <FaMapMarkerAlt
+                onClick={openLocation}
+                style={{ color: "lightpink", fontSize: "24px" }}
+              />
             </div>
           </div>
         </div>
